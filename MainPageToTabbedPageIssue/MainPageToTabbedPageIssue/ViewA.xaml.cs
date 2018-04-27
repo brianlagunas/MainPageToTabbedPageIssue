a9573b41ed671e955be5372d616cf6956617e460
Brian Lagunas <brian.lagunas@live.com>
2018-04-27 15:08:18 -0600

    Add project files.

---
+++ MainPageToTabbedPageIssue/MainPageToTabbedPageIssue/ViewA.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MainPageToTabbedPageIssue
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewA : ContentPage
+	{
+		public ViewA ()
+		{
+			InitializeComponent ();
+
+            _button.Clicked += _button_Clicked;
+		}
+
+        private void _button_Clicked(object sender, EventArgs e)
+        {
+            //build up navigation stack
+            var tabbedPage = new TabbedPage();
+            tabbedPage.Navigation.PushModalAsync(new ViewB());
+
+            //shoud have 1, this is correct
+            var numberOfPages = tabbedPage.Navigation.ModalStack.Count;
+
+            //reset main page - View should be showing, but it's a blank screen
+            App.Current.MainPage = tabbedPage;
+
+            //should have 1 page on modal stack, but now there is only 1
+            numberOfPages = tabbedPage.Navigation.ModalStack.Count;
+        }
+    }
+}
